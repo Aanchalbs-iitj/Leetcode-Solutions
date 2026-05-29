26987e5f1e72aeae81f5e80028e5a33d86769357
Aanchal Bhaskar Shukla <219551788+Aanchalbs-iitj@users.noreply.github.com>
2026-05-30 05:19:25 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (49.12%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cpp
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     vector<int> targetIndices(vector<int>& nums, int target) {
-    sort(nums.begin(),nums.end());
+    int less=0, equal=0;//O(n) time complexity and constant O(1)-sc
     vector<int> ans;
-    for(int i=0;i<nums.size();i++){
-        if (nums[i]==target){
-            ans.push_back(i);
-        }
+    for (int i=0;i<nums.size();i++){
+        if(nums[i]<target) less++;
+        if (nums[i]==target) equal++;
+    }
+    for(int i=less;i<less+equal;i++){
+        ans.push_back(i);
     }
     return ans;
     }
