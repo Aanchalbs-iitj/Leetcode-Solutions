f4f1d4a181618000c0b4e54ddd85126aa0e687ef
Aanchal Bhaskar Shukla <219551788+Aanchalbs-iitj@users.noreply.github.com>
2026-06-03 04:11:26 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (26.10%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,45 +11,43 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if (list1==NULL && list2==NULL) return NULL;
-        if (list1==NULL && list2!=NULL) return list2;
-        if (list2==NULL && list1!=NULL) return list1;
-        ListNode* temp1=list1;
-        ListNode* temp2=list2;
+        // if (list1==NULL && list2==NULL) return NULL;
+        // if (list1==NULL && list2!=NULL) return list2;
+        // if (list2==NULL && list1!=NULL) return list1;
+        // ListNode* temp1=list1;
+        // ListNode* temp2=list2;
         ListNode* dummy=new ListNode(-1);
         ListNode* curr=dummy;
-        while(temp1!=NULL || temp2!=NULL){
-            if (temp1!= NULL && temp2!=NULL){
-                if (temp1->val<=temp2->val){
-                ListNode* newNode=new ListNode(temp1->val);
-                curr->next=newNode;
-                curr=curr->next;
-                temp1=temp1->next;
+        // while(list1!=NULL || list2!=NULL){
+            while (list1!= NULL && list2!=NULL){
+                if (list1->val<=list2->val){
+                // ListNode* newNode=new ListNode(temp1->val);
+                curr->next=list1;
+                list1=list1->next;
             }
                 else {
-                ListNode* newNode=new ListNode(temp2->val);
-                curr->next=newNode;
-                curr=curr->next;
-                temp2=temp2->next;
+                // ListNode* newNode=new ListNode(temp2->val);
+                curr->next=list2;
+                // curr=curr->next;
+                list2=list2->next;
             }
-        
+            curr=curr->next;
          }
-            else{//dono mein se koi ek list end ho jaegi
-                if (temp1) {
-                ListNode* newNode=new ListNode(temp1->val);
-                curr->next=newNode;
-                curr=curr->next;
-                temp1=temp1->next;
-                }
-                else {
-                ListNode* newNode=new ListNode(temp2->val);
-                curr->next=newNode;
-                curr=curr->next;
-                temp2=temp2->next;
-                }
-            }
+        //dono mein se koi ek list end ho jaegi
+                // if (temp1) {
+                // // ListNode* newNode=new ListNode(temp1->val);
+                // curr->next=temp1;
+                // curr=curr->next;
+                // temp1=temp1->next;
+                // }
+                // else {
+                // // ListNode* newNode=new ListNode(temp2->val);
+                // curr->next=temp2;
+                // curr=curr->next;
+                // temp2=temp2->next;
+                // }
+            curr->next=(list1)? list1 :list2;
 
-        }
         ListNode* head= dummy->next;
         delete dummy;
         return head;
