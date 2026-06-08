77fa94ad24f8238273ce869a2a9583fea1def689
Aanchal Bhaskar Shukla <219551788+Aanchalbs-iitj@users.noreply.github.com>
2026-06-09 03:07:04 +0530

    Time: 53 ms (77.78%) | Memory: 24 MB (55.00%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -11,16 +11,29 @@ public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         if (headA==NULL || headB==NULL) return NULL;// agar koi ek linkedlist empty hai toh there can be no collision point
         ListNode* t1=headA;
-        ListNode* t2=headB;
-        while(t1!=t2){//initial check if the headA is not equal to headB
+        while(t1->next!=NULL){
             t1=t1->next;
-            t2=t2->next;
-
-            if (t1==t2) return t1;
-
-            if (t1==NULL) t1=headB;
-            if (t2==NULL) t2=headA;
         }
-        return t1;//if there is same linkedlist headA and headB will collide
+        //now t1 is at the tail
+        t1->next=headB;
+        ListNode* t3=headA;//slow
+        ListNode* t2=headA;//fast
+        while(t2!=NULL && t2->next!=NULL){//initial check if the headA is not equal to headB
+            t3=t3->next;
+            t2=t2->next->next;
+
+            if (t3==t2){
+                t3=headA;
+                while(t3!=t2){
+                    t3=t3->next;
+                    t2=t2->next;
+                }
+                t1->next=NULL;
+                return t3;
+            };
+
+        }
+        t1->next=NULL;
+        return NULL;//if there is same linkedlist headA and headB will collide
     }
 };
