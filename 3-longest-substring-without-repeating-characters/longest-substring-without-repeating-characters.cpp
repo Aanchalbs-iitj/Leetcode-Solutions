8b3fa7ab3a6db11fe326f41d138ca7aae1bb5f02
Aanchal Bhaskar Shukla <219551788+Aanchalbs-iitj@users.noreply.github.com>
2026-07-01 03:11:41 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (76.85%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        //brute force
+        //using sliding window and hash table
         int n=s.size();
-        int maxlength=0;
-        for(int i=0;i<n;i++){
-            vector<int> hash(256,0);
-            for (int j=i;j<n;j++){
-                if (hash[s[j]]==1){// we have encountered this character before and agar isko le liya toh s[i] se start hone wali aagi ki sari strings mein ye aaega so break from it
-                break;
+        int l=0,r=0,maxlength=0;
+        vector<int> hash(256,-1);
+        while(r<n){
+            if (hash[s[r]]!=-1){
+                if (hash[s[r]]>=l){
+                    l=hash[s[r]]+1;
                 }
-            int len=j-i+1;
-            maxlength=max(maxlength,len);
-            hash[s[j]]=1;
             }
+            int len=r-l+1;
+            maxlength=max(maxlength,len);
+            hash[s[r]]=r;//update for furthur window
+            r++;
         }
         return maxlength;
     }
