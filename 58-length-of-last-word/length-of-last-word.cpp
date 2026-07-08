f1195f852c0dfadeb59906b2a084d3f98706b7a9
Aanchal Bhaskar Shukla <219551788+Aanchalbs-iitj@users.noreply.github.com>
2026-07-09 02:01:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (68.26%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int n=s.size();
-        bool flag=false;//to detect non empty character
+        int i=s.size()-1;
+        while(i>=0 && s[i]==' ') i--;
         int count=0;
-        for(int i=n-1;i>=0;i--){
-            if(s[i]==' ') {
-                flag=true;
-            }
-            else {
-                count++;
-                flag=false;
-            }
-            if (flag==true && i+1<=n-1 && s[i+1]!=' ') break;
+        while(i>=0 && s[i]!=' '){
+            count++;
+            i--;
         }
         return count;
     }
