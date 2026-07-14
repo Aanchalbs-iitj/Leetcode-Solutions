972dd13170cbe4fbabcf6c3fccbc192826d27451
Aanchal Bhaskar Shukla <219551788+Aanchalbs-iitj@users.noreply.github.com>
2026-07-15 03:58:26 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (52.73%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
-    int tribonacci(int n) {//tabulation - bottom up
+    int tribonacci(int n) {//constant space and three pointers
         if (n==0) return 0;
         if (n==1 ||n==2) return 1;
-        vector<int> dp(n+1,0);
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=1;
+        
+        int a=0;
+        int b=1;
+        int c=1;
         for(int i=3;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+            int d=a+b+c;
+            a=b;
+            b=c;
+            c=d;
         }
-        return dp[n];
+        return c;
     }
 };
