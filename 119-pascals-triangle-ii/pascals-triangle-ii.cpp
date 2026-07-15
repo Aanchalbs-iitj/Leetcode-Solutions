43ab6856e8141681758d824843ed2c246332fbb1
Aanchal Bhaskar Shukla <219551788+Aanchalbs-iitj@users.noreply.github.com>
2026-07-15 21:13:17 +0530

    Time: 2 ms (9.61%) | Memory: 8.9 MB (74.46%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<int> getRow(int rowIndex) {
         vector<int> row;
+        row.reserve(rowIndex + 1);
         long long res=1;
         row.push_back(res);
         for(int i=1;i<rowIndex+1;i++){
