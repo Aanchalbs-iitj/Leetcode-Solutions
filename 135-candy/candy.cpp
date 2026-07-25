3230b72cb326a214d74a1d5caf2401d5ca7404fc
Aanchal Bhaskar Shukla <219551788+Aanchalbs-iitj@users.noreply.github.com>
2026-07-26 02:40:40 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (52.54%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -3,25 +3,29 @@ public:
     int candy(vector<int>& ratings) {
         int n=ratings.size();
         vector<int> left(n);
-        vector<int> right(n);
+        // vector<int> right(n);
         left[0]=1;
-        right[n-1]=1;//boundary and there is no right neighbour
+        // right[n-1]=1;//boundary and there is no right neighbour
         for(int i=1;i<n;i++){//for left vector
             if (ratings[i]>ratings[i-1]){
                 left[i]=left[i-1]+1;
             }
             else left[i]=1;
         }
+        int right=1;
+        int sum=max(1,left[n-1]);
         for(int i=n-2;i>=0;i--){
             if(ratings[i]>ratings[i+1]){
-                right[i]=right[i+1]+1;
+                right++;
             }
-            else right[i]=1;
+            else right=1;
+            // else right[i]=1;
+            sum+=max(left[i],right);
         }
-        int candies=0;
-        for(int i=0;i<n;i++){
-            candies+=max(left[i],right[i]);
-        }
-        return candies;
+        // int candies=0;
+        // for(int i=0;i<n;i++){
+        //     candies+=max(left[i],right[i]);
+        // }
+        return sum;
     }
 };
