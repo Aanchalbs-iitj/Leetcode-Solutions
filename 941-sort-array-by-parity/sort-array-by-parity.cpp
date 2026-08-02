c702a56c01b0cf7d4877327ac25c2d833a84610b
Aanchal Bhaskar Shukla <219551788+Aanchalbs-iitj@users.noreply.github.com>
2026-08-02 07:13:21 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (54.16%) - LeetSync

---
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1) return nums;
+        vector<int> ans(n,0);
+        int epos=0;
+        int opos=n-1;
+        for(int i=0;i<n;i++){
+            if(nums[i]%2==0) {
+                ans[epos]=nums[i];
+                epos++;
+            }
+            else{
+                ans[opos]=nums[i];
+                opos--;
+            }
+        }
+        return ans;
+    }
+};
