58837e71baf107c140c751a1a9c16eebaeac6b38
Aanchal Bhaskar Shukla <219551788+Aanchalbs-iitj@users.noreply.github.com>
2026-08-06 07:59:28 +0530

    Time: 4 ms (66.11%) | Memory: 23.9 MB (59.50%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {//brute way
-    int n = intervals.size();
-    sort(intervals.begin(), intervals.end());
-    vector<vector<int>> ans;
-    for (int i = 0; i < n; i++) {
-        int start = intervals[i][0];
-        int end = intervals[i][1];
-        if (!ans.empty() && end <= ans.back()[1]) {//already the current interval is inside the ith interval
-            continue;
-        }
-        for (int j = i + 1; j < n; j++) {
-            if (intervals[j][0] <= end) {
-                end = max(end, intervals[j][1]);
-            } else {
-                break;
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        //optimal way
+        int n=intervals.size();
+        vector<vector<int>> ans;
+        sort(intervals.begin(),intervals.end());
+        for(int i=0;i<n;i++){
+            if(ans.empty() || intervals[i][0]>ans.back()[1]){
+                ans.push_back(intervals[i]);
+            }
+            else {
+                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
             }
         }
-       ans.push_back({start, end});
-    }
-    return ans;
+        return ans;
     }
 };
-//every subinterval is checked twice or compared twice 
