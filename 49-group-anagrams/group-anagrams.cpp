077afc9158de63e5a9c13ab485dd5d6fcb11b8be
Aanchal Bhaskar Shukla <219551788+Aanchalbs-iitj@users.noreply.github.com>
2026-08-07 08:23:12 +0530

    Time: 27 ms (22.46%) | Memory: 30 MB (17.28%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        // group by sorting
+        // group by frequency- it counts the frequency of each character and uses that frequency array as the key.
         unordered_map<string,vector<string>> mp;
         for (int i=0;i<strs.size();i++){
             string s=strs[i];//copy the string cause we have to push the original string later
-            sort(s.begin(),s.end());//sort it
-            mp[s].push_back(strs[i]);
+            vector<int> freq(26,0);
+            for(char c : s){// counting freq
+                freq[c-'a']++;
+            }
+            //building key
+            string key="";
+            for(int i=0;i<26;i++){
+                if(freq[i]>0){
+                    key+=char(i+'a');
+                    key+=to_string(freq[i]);
+                }
+            }
+            mp[key].push_back(s);
         }
         vector<vector<string>> ans;
         for(auto &it:mp){ //works with & and not & too
